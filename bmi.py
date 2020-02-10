50a1a326a59ab1ab853f6eeb55b3b7d447cb27ce
Eoin <eoinlees@outlook.com>
2020-02-10 10:58:55 +0000

    Added task 1 following video about problem sheet

---
+++ bmi.py
@@ -0,0 +1,22 @@
+# Eoin Lees
+# This program calculates a persons Body Mass Index.
+
+weight = float(input("Enter weight in kg: "))
+height = float(input("Enter height in cm: "))
+
+#weight = 65
+#height = 180
+
+#convert Height to meters
+
+hm = height / 100
+
+#square height
+
+hsqr = (hm * hm)
+
+bmi = round(weight/hsqr,2)
+
+print("Your bmi is:", bmi)
+
+#Notes: review lecture 1 for basics on variables / inputs etc.. 
