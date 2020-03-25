fdd449a49f0067a4a0a0fdce47dd8fe393b9127a
Eoin <eoinlees@outlook.com>
2020-03-25 22:53:11 +0000

    Adding week 6 problem

---
+++ 6-squareroot.py
@@ -0,0 +1,12 @@
+# Eoin Lees
+# This program takes a positive floating-point number as input
+# and outputs an approximation of its square root
+# by creating a function 
+
+num = float(input("Please enter a positive number: "))
+
+if num <= 0:   
+    print("You must enter a positive number for the program to run")
+
+#Test
+print(num)
