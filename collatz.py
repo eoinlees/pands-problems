673c71b0a1e1db721a8996a6eed4e98f7c1504db
Eoin <eoinlees@outlook.com>
2020-03-25 15:10:19 +0000

    Added Week 4 task

---
+++ collatz.py
@@ -0,0 +1,6 @@
+#Eoin Lees
+# This program inputs a positive integer and outputs the value of it if
+# value even divide by two
+# value odd mulitply it by three and add one
+# program ends when current value is 1
+
