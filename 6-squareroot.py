a7e52cdb0f712f5d1a8704b2bc30cbd4789a5bc8
Eoin <eoinlees@outlook.com>
2020-03-26 16:49:44 +0000

    Tidied up code

--- 6-squareroot.py
+++ 6-squareroot.py
@@ -3,8 +3,10 @@
 # and outputs an approximation of its square root
 # by creating a function 
 
+# Get float input for square root calculation
 num = float(input("Please enter a positive number: "))
 
+# Define function
 def newtonSqr(number, numberIterations = 300):
     a = float(number)
     for i in range(numberIterations):
@@ -14,8 +16,9 @@ def newtonSqr(number, numberIterations = 300):
 # Followed example uses here : https://medium.com/@sddkal/newton-square-root-method-in-python-270853e9185d
 
 ans = newtonSqr(num)
-
+# Ensure Positive number
 if num <= 0:   
     print("You must enter a positive number for the program to run")
+# Print result if positive
 else:
     print("The square root of {} is approx. {}".format(num,ans))
