666e41c9cff53eab320e7547ddb0a938d3bdbbca
Eoin <eoinlees@outlook.com>
2020-03-26 17:40:40 +0000

    Finished week 7 task. Will return to improve upon

--- 7-es.py
+++ 7-es.py
@@ -3,3 +3,19 @@
 # the number of e's it contains. 
 # It reads the file name from the command line argument.
 
+# Get document from argument
+import sys
+with open(sys.argv[1],"r") as file: # Open the file in read mode
+    data = file.read() # Read the file
+
+# Use count function to count substring "e"
+numE = data.count("e")
+
+# Site referenced for data import https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
+# Sites referenced for count https://www.geeksforgeeks.org/python-string-count/
+
+# Print number of e's to 
+print(numE)
+
+# Complete for now. Return to enter if statement 
+# reguarding no text file added to python command
