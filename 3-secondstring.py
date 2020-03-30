5325fe92ac9a37262101dd7593e09913594704e4
Eoin <eoinlees@outlook.com>
2020-03-30 16:56:26 +0100

    Week 3 task complete

--- 3-secondstring.py
+++ 3-secondstring.py
@@ -1,8 +1,10 @@
-#Eoin Lees
+# Eoin Lees
 # This program inputs a string and prints every second
 # letter in reverse order.
 
+# Get input from user and assign it as string
 string = input("Please enter a sentence: ")
 
-#List indexing and slicing - A whrlwind tour of python Page 32
+# Print input following alteration.
+# List indexing and slicing - A whrlwind tour of python Page 32
 print(string[::-2])
