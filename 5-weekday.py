34da37d3146d5ead6306b902e72cc7f2329557f0
Eoin <eoinlees@outlook.com>
2020-03-30 16:27:12 +0100

    Week 5 task complete

--- 5-weekday.py
+++ 5-weekday.py
@@ -2,20 +2,22 @@
 # This program outputs whether today is a weekday or not. 
 # It runs dependant on the day of the week
 
+# Import datetime function to python
 import datetime
-import calendar
 
+# Create tuple to assign days of week in words. 
 L = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
 
+# Get day from imported functions
 today = datetime.datetime.today().weekday()
 
+# Assign name to day number imported
 dayName = L[today]
 print("Today is {}".format(dayName))
 
+# If statement to print depending of if weekend or not
 if today < 5:
     print("Yes, unfortunately today is a weekday.")
 
 else:
     print("It is the weekend, yay!")
-
-#Note: Test on weekend - ensure program works each day. 
