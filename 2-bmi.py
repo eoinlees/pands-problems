2c0ac7df335d23a54b04e82731cbd0c5f015d4c5
Eoin <eoinlees@outlook.com>
2020-03-30 16:51:27 +0100

    Week 2 task complete

--- 2-bmi.py
+++ 2-bmi.py
@@ -1,24 +1,28 @@
 # Eoin Lees
 # This program calculates a persons Body Mass Index.
 
+# Get the inputs from the user
 weight = float(input("Enter weight in kg: "))
 height = float(input("Enter height in cm: "))
 
-#weight = 65
-#height = 180
-
-#convert Height to meters
-
+# Convert Height to meters
 hm = height / 100
 
-#square height
-
+# Square height
 hsqr = (hm * hm)
 
+# Calculate bmi
 bmi = round(weight/hsqr,2)
 
+# Print result
 print("Your bmi is:", bmi)
 
-#Notes: review lecture 1 for basics on variables / inputs etc.. 
-# Add in bmi definitions? If over then etc...
-
+# Extra if statement using WHO bmi classifications
+if bmi < 18.50:
+    print("According to the WHO You are Underweight.")
+elif 18.50 < bmi < 24.99:
+    print("According to the WHO You are normal weight.")
+elif 25 < bmi < 29.99:
+    print("According to the WHO You are overweight.")
+else:
+    print("According to the WHO You are obese.")
