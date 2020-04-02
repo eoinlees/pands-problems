88d50aa4005e65e517e05b49055a36bb04143bf6
Eoin <eoinlees@outlook.com>
2020-04-02 23:06:04 +0100

    Weekly Task 4 complete

--- 4-collatz.py
+++ 4-collatz.py
@@ -4,11 +4,16 @@
 # value odd mulitply it by three and add one
 # program ends when current value is 1
 
+# Get positive value from input
 x = int(input("Please enter a posotive integer: "))
 
+# Return  
 if x <= 0:
     print("Please enter a positive number.")
-elif x % 2 == 0
-    print(x)
-
-# finish on 31 03/20
+# Use while loop to ensure value is positive and greater than 1. Ends once value is 1. 
+while x > 1:   
+    if x % 2 == 0: # Divides value by 2 if even
+        x = (x/2)
+    else:  # multiplies by 3 and adds 1 if off
+        x = ((x * 3)+1)
+    print(x) # Prints value of input following calculation. restarts loop until x = 1
